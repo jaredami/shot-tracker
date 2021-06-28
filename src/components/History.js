0b9ac533bd05c8a8c3f46c531d602c6ba55f01ee
Jared Storm <jaredrstorm@gmail.com>
2021-06-27 20:17:34 -0400

    Basic styles for History

--- src/components/History.js
+++ src/components/History.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./History.css";
+import * as styles from "./History.module.css";
 
 const sessions = [
   {
@@ -37,17 +37,63 @@ const sessions = [
     shotsMade: "43/100",
     bestStreak: "9",
   },
+  {
+    id: 6,
+    date: "5/22/21",
+    percentage: "43",
+    shotsMade: "43/100",
+    bestStreak: "9",
+  },
+  {
+    id: 7,
+    date: "5/22/21",
+    percentage: "43",
+    shotsMade: "43/100",
+    bestStreak: "9",
+  },
+  {
+    id: 8,
+    date: "5/22/21",
+    percentage: "43",
+    shotsMade: "43/100",
+    bestStreak: "9",
+  },
+  {
+    id: 9,
+    date: "5/22/21",
+    percentage: "43",
+    shotsMade: "43/100",
+    bestStreak: "9",
+  },
+  {
+    id: 10,
+    date: "5/22/21",
+    percentage: "43",
+    shotsMade: "43/100",
+    bestStreak: "9",
+  },
 ];
 
 export default function History() {
   return (
-    <div>
+    <div className={styles.historyContainer}>
       {sessions.map((session) => (
-        <div className="session" key={session.id}>
-          <div>{session.date}</div>
-          <div>{session.percentage}</div>
-          <div>{session.shotsMade}</div>
-          <div>{session.bestStreak}</div>
+        <div className={styles.session} key={session.id}>
+          <div className={styles.date}>{session.date}</div>
+          <div className={styles.statsContainer}>
+            <div className={styles.statContainer}>
+              <div className={styles.stat}>{session.percentage}</div>
+              <div className={styles.statLabel}>Percentage</div>
+            </div>
+            <div className={styles.statContainer}>
+              <div className={styles.stat}>{session.shotsMade}</div>
+              <div className={styles.statLabel}>Shots Made</div>
+            </div>
+            <div className={styles.statContainer}>
+              <div className={styles.stat}>{session.bestStreak}</div>
+              <div className={styles.statLabel}>Best Streak</div>
+            </div>
+          </div>
         </div>
       ))}
     </div>
