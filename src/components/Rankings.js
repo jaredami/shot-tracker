8d0bc33961b6f7288fecc8f9df910015215ce31c
Jared Storm <jaredrstorm@gmail.com>
2021-07-23 13:26:27 -0400

    Sort rankings by selected stat

--- src/components/Rankings.js
+++ src/components/Rankings.js
@@ -60,18 +60,22 @@ export default function Rankings() {
         </div>
       </div>
       <div className={styles.rankingsContainer}>
-        {data.map((user, index) => (
-          <div key={user.userId} className={styles.userCard}>
-            <div className={styles.rank}>{index + 1}</div>
-            <div className={styles.userInfoContainer}>
-              <div className={styles.userPicContainer}>
-                <i className="fas fa-user"></i>
+        {data
+          .sort((a, b) =>
+            a[currentStat.value] < b[currentStat.value] ? 1 : -1
+          )
+          .map((user, index) => (
+            <div key={user.userId} className={styles.userCard}>
+              <div className={styles.rank}>{index + 1}</div>
+              <div className={styles.userInfoContainer}>
+                <div className={styles.userPicContainer}>
+                  <i className="fas fa-user"></i>
+                </div>
+                <div>{user.userName}</div>
+                <div>{user[currentStat.value]}</div>
               </div>
-              <div>{user.userName}</div>
-              <div>{user[currentStat.value]}</div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </>
   );
@@ -82,7 +86,7 @@ const data = [
     userId: 1,
     userName: "Domantas Sabonis",
     percentage: 56,
-    shotsMade: 33,
+    shotsMade: 56,
     shotsTaken: 100,
     bestStreak: 7,
     sessionCount: 1,
@@ -91,52 +95,52 @@ const data = [
     userId: 2,
     userName: "Goga Bitadze",
     percentage: 55,
-    shotsMade: 33,
+    shotsMade: 55,
     shotsTaken: 100,
-    bestStreak: 7,
+    bestStreak: 5,
     sessionCount: 1,
   },
   {
     userId: 3,
     userName: "Malcolm Brogdon",
     percentage: 54,
-    shotsMade: 33,
+    shotsMade: 54,
     shotsTaken: 100,
-    bestStreak: 7,
+    bestStreak: 8,
     sessionCount: 1,
   },
   {
     userId: 4,
     userName: "Aaron Holiday",
     percentage: 52,
-    shotsMade: 33,
+    shotsMade: 52,
     shotsTaken: 100,
-    bestStreak: 7,
+    bestStreak: 10,
     sessionCount: 1,
   },
   {
     userId: 5,
     userName: "T.J. McConnell",
     percentage: 50,
-    shotsMade: 33,
+    shotsMade: 50,
     shotsTaken: 100,
-    bestStreak: 7,
+    bestStreak: 9,
     sessionCount: 1,
   },
   {
     userId: 6,
     userName: "Justin Holiday",
     percentage: 48,
-    shotsMade: 33,
+    shotsMade: 48,
     shotsTaken: 100,
-    bestStreak: 7,
+    bestStreak: 9,
     sessionCount: 1,
   },
   {
     userId: 7,
     userName: "Doug McDermott",
     percentage: 47,
-    shotsMade: 33,
+    shotsMade: 47,
     shotsTaken: 100,
     bestStreak: 7,
     sessionCount: 1,
@@ -145,16 +149,16 @@ const data = [
     userId: 8,
     userName: "T.J. Warren",
     percentage: 45,
-    shotsMade: 33,
+    shotsMade: 45,
     shotsTaken: 100,
-    bestStreak: 7,
+    bestStreak: 6,
     sessionCount: 1,
   },
   {
     userId: 9,
     userName: "Myles Turner",
     percentage: 42,
-    shotsMade: 33,
+    shotsMade: 42,
     shotsTaken: 100,
     bestStreak: 7,
     sessionCount: 1,
@@ -163,27 +167,27 @@ const data = [
     userId: 10,
     userName: "Jeremy Lamb",
     percentage: 42,
-    shotsMade: 33,
+    shotsMade: 42,
     shotsTaken: 100,
-    bestStreak: 7,
+    bestStreak: 4,
     sessionCount: 1,
   },
   {
     userId: 11,
     userName: "JaKarr Sampson",
     percentage: 41,
-    shotsMade: 33,
-    shotsTaken: 100,
-    bestStreak: 7,
+    shotsMade: 82,
+    shotsTaken: 200,
+    bestStreak: 6,
     sessionCount: 1,
   },
   {
     userId: 12,
     userName: "Edmond Sumner",
     percentage: 39,
-    shotsMade: 33,
-    shotsTaken: 100,
-    bestStreak: 7,
+    shotsMade: 117,
+    shotsTaken: 300,
+    bestStreak: 11,
     sessionCount: 1,
   },
 ];
