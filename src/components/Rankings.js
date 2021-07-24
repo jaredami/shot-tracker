e9e6045981ce5b551579499ca1c83ef977b63514
Jared Storm <jaredrstorm@gmail.com>
2021-07-24 15:05:45 -0400

    Move mock data to separate file

--- src/components/Rankings.js
+++ src/components/Rankings.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { playerData } from "../data/db";
 import * as styles from "./Rankings.module.css";
 
 export default function Rankings() {
@@ -60,7 +61,7 @@ export default function Rankings() {
         </div>
       </div>
       <div className={styles.rankingsContainer}>
-        {data
+        {playerData
           .sort((a, b) =>
             a[currentStat.value] < b[currentStat.value] ? 1 : -1
           )
@@ -80,114 +81,3 @@ export default function Rankings() {
     </>
   );
 }
-
-const data = [
-  {
-    userId: 1,
-    userName: "Domantas Sabonis",
-    percentage: 56,
-    shotsMade: 56,
-    shotsTaken: 100,
-    bestStreak: 7,
-    sessionCount: 1,
-  },
-  {
-    userId: 2,
-    userName: "Goga Bitadze",
-    percentage: 55,
-    shotsMade: 55,
-    shotsTaken: 100,
-    bestStreak: 5,
-    sessionCount: 1,
-  },
-  {
-    userId: 3,
-    userName: "Malcolm Brogdon",
-    percentage: 54,
-    shotsMade: 54,
-    shotsTaken: 100,
-    bestStreak: 8,
-    sessionCount: 1,
-  },
-  {
-    userId: 4,
-    userName: "Aaron Holiday",
-    percentage: 52,
-    shotsMade: 52,
-    shotsTaken: 100,
-    bestStreak: 10,
-    sessionCount: 1,
-  },
-  {
-    userId: 5,
-    userName: "T.J. McConnell",
-    percentage: 50,
-    shotsMade: 50,
-    shotsTaken: 100,
-    bestStreak: 9,
-    sessionCount: 1,
-  },
-  {
-    userId: 6,
-    userName: "Justin Holiday",
-    percentage: 48,
-    shotsMade: 48,
-    shotsTaken: 100,
-    bestStreak: 9,
-    sessionCount: 1,
-  },
-  {
-    userId: 7,
-    userName: "Doug McDermott",
-    percentage: 47,
-    shotsMade: 47,
-    shotsTaken: 100,
-    bestStreak: 7,
-    sessionCount: 1,
-  },
-  {
-    userId: 8,
-    userName: "T.J. Warren",
-    percentage: 45,
-    shotsMade: 45,
-    shotsTaken: 100,
-    bestStreak: 6,
-    sessionCount: 1,
-  },
-  {
-    userId: 9,
-    userName: "Myles Turner",
-    percentage: 42,
-    shotsMade: 42,
-    shotsTaken: 100,
-    bestStreak: 7,
-    sessionCount: 1,
-  },
-  {
-    userId: 10,
-    userName: "Jeremy Lamb",
-    percentage: 42,
-    shotsMade: 42,
-    shotsTaken: 100,
-    bestStreak: 4,
-    sessionCount: 1,
-  },
-  {
-    userId: 11,
-    userName: "JaKarr Sampson",
-    percentage: 41,
-    shotsMade: 82,
-    shotsTaken: 200,
-    bestStreak: 6,
-    sessionCount: 1,
-  },
-  {
-    userId: 12,
-    userName: "Edmond Sumner",
-    percentage: 39,
-    shotsMade: 117,
-    shotsTaken: 300,
-    bestStreak: 11,
-    sessionCount: 1,
-  },
-];
