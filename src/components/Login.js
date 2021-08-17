410ba385934681080f54a40ea13b1ab8d1588474
Jared Storm <jaredrstorm@gmail.com>
2021-08-16 21:34:08 -0400

    Redirect to /train route on log in

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,7 +17,7 @@ export default function Login() {
       setError("");
       setLoading(true);
       await login(emailRef.current.value, passwordRef.current.value);
-      history.push("/");
+      history.push("/train");
     } catch {
       setError("Failed to log in");
     }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -22,7 +22,7 @@ export default function Signup() {
       setError("");
       setLoading(true);
       await signup(emailRef.current.value, passwordRef.current.value);
-      history.push("/");
+      history.push("/train");
     } catch {
       setError("Failed to create an account");
     }
