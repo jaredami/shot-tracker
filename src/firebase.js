1774eed03daeb63816501cf290abed3127071f4a
Jared Storm <jaredrstorm@gmail.com>
2021-09-18 13:18:24 -0400

    Refactor firebase config file

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-const app = firebase.initializeApp({
+const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   // databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
@@ -10,7 +10,8 @@ const app = firebase.initializeApp({
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-});
+};
+firebase.initializeApp(config);
 
-export const auth = app.auth();
+export const auth = firebase.auth();
 export const db = firebase.firestore();
