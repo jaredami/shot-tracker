a629c2b5293e51b0eb23e5fa6c6d71f601646c3d
Jared Storm <jaredrstorm@gmail.com>
2021-09-19 14:12:22 -0400

    Style modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,5 +2,11 @@ import React from "react";
 import "./Modal.css";
 
 export default function Modal() {
-  return <div className="modal-container"></div>;
+  return (
+    <div className="modal__container">
+      <p className="modal__message">Message</p>
+      <button className="modal__button modal__button--yes">Yes</button>
+      <button className="modal__button modal__button--no">No</button>
+    </div>
+  );
 }
