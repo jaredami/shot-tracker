29b5e783bd1f6ddbf0bd3290774fa043e7220799
Jared Storm <jaredrstorm@gmail.com>
2021-09-19 14:13:24 -0400

    Add message prop to Modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./Modal.css";
 
-export default function Modal() {
+export default function Modal({ message }) {
   return (
     <div className="modal__container">
-      <p className="modal__message">Message</p>
+      <p className="modal__message">{message ? message : "Are you sure?"}</p>
       <button className="modal__button modal__button--yes">Yes</button>
       <button className="modal__button modal__button--no">No</button>
     </div>
