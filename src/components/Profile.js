f33f333e3a5f69d6eff3ab38513763e2ea11b1d1
Jared Storm <jaredrstorm@gmail.com>
2021-09-26 18:39:04 -0400

    Begin styling profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useAuth } from "../contexts/AuthContext";
 import { Link, useHistory } from "react-router-dom";
+import "./Profile.css";
 
 export default function Dashboard() {
   const [error, setError] = useState("");
@@ -18,8 +19,38 @@ export default function Dashboard() {
     }
   }
 
+  const [shotsTakenCount, setShotsTakenCount] = useState(0);
+  const [shotsMadeCount, setShotsMadeCount] = useState(0);
+  const [bestStreak, setBestStreak] = useState(0);
+
+  function getPercentage() {
+    return shotsTakenCount
+      ? Math.floor((shotsMadeCount / shotsTakenCount) * 100) + "%"
+      : "--";
+  }
+
   return (
-    <>
+    <div className="profile-container">
+      <div className="session-grid">
+        <div className="stat-container">
+          <p className="stat">23</p>
+          <p className="stat-label">Sessions</p>
+        </div>
+        <div className="stat-container">
+          <p className="stat">
+            {shotsMadeCount}/{shotsTakenCount}
+          </p>
+          <p className="stat-label">Shots Made</p>
+        </div>
+        <div className="stat-container">
+          <p className="stat">{getPercentage()}</p>
+          <p className="stat-label">Percentage</p>
+        </div>
+        <div className="stat-container">
+          <p className="stat">{bestStreak}</p>
+          <p className="stat-label">Best Streak</p>
+        </div>
+      </div>
       <div>
         <div>
           {error && <div variant="danger">{error}</div>}
@@ -34,6 +65,6 @@ export default function Dashboard() {
           Log Out
         </button>
       </div>
-    </>
+    </div>
   );
 }
