66d1f1919a38751f9387c2e29fcd1feb74e78df2
Jared Storm <jaredrstorm@gmail.com>
2021-10-16 11:32:59 -0400

    Load sessions data in Rankings

--- src/components/Rankings.js
+++ src/components/Rankings.js
@@ -1,13 +1,34 @@
 import React, { useState } from "react";
+import { db } from "../firebase";
 import { playerData } from "../mock-data/db";
+import LoadingIndicator from "./LoadingIndicator";
 import * as styles from "./Rankings.module.css";
 
 export default function Rankings() {
+  const [isLoading, setIsLoading] = useState(false);
   const [isDropdownExpanded, setIsDropdownExpanded] = useState(false);
   const [currentStat, setCurrentStat] = useState({
     label: "Percentage",
     value: "percentage",
   });
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+
+      await db
+        .collection("sessions")
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            console.log("doc.data()", doc.data());
+          });
+        });
+
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
 
   function toggleDropdown() {
     setIsDropdownExpanded(!isDropdownExpanded);
@@ -60,24 +81,27 @@ export default function Rankings() {
           </div>
         </div>
       </div>
-      <div className={styles.rankingsContainer}>
-        {playerData
-          .sort((a, b) =>
-            a[currentStat.value] < b[currentStat.value] ? 1 : -1
-          )
-          .map((user, index) => (
-            <div key={user.userId} className={styles.userCard}>
-              <div className={styles.rank}>{index + 1}</div>
-              <div className={styles.userInfoContainer}>
-                <div className={styles.userPicContainer}>
-                  <i className="fas fa-user"></i>
+      {isLoading && <LoadingIndicator />}
+      {!isLoading && (
+        <div className={styles.rankingsContainer}>
+          {playerData
+            .sort((a, b) =>
+              a[currentStat.value] < b[currentStat.value] ? 1 : -1
+            )
+            .map((user, index) => (
+              <div key={user.userId} className={styles.userCard}>
+                <div className={styles.rank}>{index + 1}</div>
+                <div className={styles.userInfoContainer}>
+                  <div className={styles.userPicContainer}>
+                    <i className="fas fa-user"></i>
+                  </div>
+                  <div>{user.userName}</div>
+                  <div>{user[currentStat.value]}</div>
                 </div>
-                <div>{user.userName}</div>
-                <div>{user[currentStat.value]}</div>
               </div>
-            </div>
-          ))}
-      </div>
+            ))}
+        </div>
+      )}
     </>
   );
 }
