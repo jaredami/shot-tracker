d52be02b95a361555c05048c6545552aaa9db91c
Jared Storm <jaredrstorm@gmail.com>
2021-10-24 21:51:12 -0400

    Get user data from AuthContext in Profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,13 +1,12 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
-import { db } from "../firebase";
 import LoadingIndicator from "./LoadingIndicator";
 import "./Profile.css";
 
 export default function Dashboard() {
   const [error, setError] = useState("");
-  const { currentUser, logout } = useAuth();
+  const { currentUser, logout, userData, loading } = useAuth();
   const history = useHistory();
 
   async function handleLogout() {
@@ -21,51 +20,30 @@ export default function Dashboard() {
     }
   }
 
-  const [isLoading, setIsLoading] = useState(false);
-  const [user, setUser] = useState();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-      await db
-        .collection("users")
-        .doc(currentUser.uid)
-        .get()
-        .then((docRef) => {
-          setUser(docRef.data());
-          setIsLoading(false);
-        })
-        .catch((error) => {
-          console.warn(error);
-        });
-    };
-    fetchData();
-  }, [currentUser.uid]);
-
   return (
     <>
-      {isLoading ? (
+      {loading ? (
         <LoadingIndicator />
       ) : (
-        user && (
+        userData && (
           <div className="profile-container">
             <div className="profile-grid">
               <div className="stat-container">
-                <p className="stat">{user.totalSessions}</p>
+                <p className="stat">{userData.totalSessions}</p>
                 <p className="stat-label">Sessions</p>
               </div>
               <div className="stat-container">
                 <p className="stat">
-                  {user.totalShotsMade}/{user.totalShotsTaken}
+                  {userData.totalShotsMade}/{userData.totalShotsTaken}
                 </p>
                 <p className="stat-label">Shots Made</p>
               </div>
               <div className="stat-container">
-                <p className="stat">{user.totalPercentage}</p>
+                <p className="stat">{userData.totalPercentage}</p>
                 <p className="stat-label">Percentage</p>
               </div>
               <div className="stat-container">
-                <p className="stat">{user.bestStreakEver}</p>
+                <p className="stat">{userData.bestStreakEver}</p>
                 <p className="stat-label">Best Streak</p>
               </div>
             </div>
