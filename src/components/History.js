7a28e54ae6cb81abfd789264aad621181782c569
Jared Storm <jaredrstorm@gmail.com>
2021-10-29 15:39:42 -0400

    Animate sessions list in History

--- src/components/History.js
+++ src/components/History.js
@@ -39,34 +39,39 @@ export default function History() {
     fetchData();
   }, [currentUser.uid]);
 
+  const testClass = "testClass";
+
   return (
     <div className={styles.historyContainer}>
       {isLoading ? (
         <LoadingIndicator />
       ) : sessions && sessions.length ? (
-        sessions.map((session) => (
-          <div className={styles.session} key={session.id}>
-            <div className={styles.date}>{session.date}</div>
-            <div className={styles.statsContainer}>
-              <div className={styles.statContainer}>
-                <div className={styles.stat}>{session.percentage}</div>
-                <div className={styles.statLabel}>Percentage</div>
-              </div>
-              <div className={styles.statContainer}>
-                <div className={styles.stat}>{session.shotsMade}</div>
-                <div className={styles.statLabel}>Shots Made</div>
-              </div>
-              <div className={styles.statContainer}>
-                <div className={styles.stat}>{session.shotsTaken}</div>
-                <div className={styles.statLabel}>Shots Taken</div>
-              </div>
-              <div className={styles.statContainer}>
-                <div className={styles.stat}>{session.bestStreak}</div>
-                <div className={styles.statLabel}>Best Streak</div>
+        sessions.map((session, index) => {
+          const style = { "--animation-order": index + 1 };
+          return (
+            <div style={style} className={styles.session} key={session.id}>
+              <div className={styles.date}>{session.date}</div>
+              <div className={styles.statsContainer}>
+                <div className={styles.statContainer}>
+                  <div className={styles.stat}>{session.percentage}</div>
+                  <div className={styles.statLabel}>Percentage</div>
+                </div>
+                <div className={styles.statContainer}>
+                  <div className={styles.stat}>{session.shotsMade}</div>
+                  <div className={styles.statLabel}>Shots Made</div>
+                </div>
+                <div className={styles.statContainer}>
+                  <div className={styles.stat}>{session.shotsTaken}</div>
+                  <div className={styles.statLabel}>Shots Taken</div>
+                </div>
+                <div className={styles.statContainer}>
+                  <div className={styles.stat}>{session.bestStreak}</div>
+                  <div className={styles.statLabel}>Best Streak</div>
+                </div>
               </div>
             </div>
-          </div>
-        ))
+          );
+        })
       ) : (
         <div>No Sessions Logged</div>
       )}
