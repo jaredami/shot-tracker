e8935689febb1c0b33806e24abf939c758897cca
Jared Storm <jaredrstorm@gmail.com>
2021-10-28 23:13:25 -0400

    Display current user name in header

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,20 +1,25 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
 import "./PageHeader.css";
 
 export default function PageHeader({ pageTitle, displaysProfileBtn }) {
+  const { currentUserData } = useAuth();
   return (
     <div className="header-container">
       <div className="header-content-container">
         <h1 className="header">{pageTitle}</h1>
-        {displaysProfileBtn && (
-          <NavLink
-            className="profile-icon"
-            activeClassName="profile-link-active"
-            to="/profile"
-          >
-            <i className="fas fa-user-circle"></i>
-          </NavLink>
+        {displaysProfileBtn && currentUserData && (
+          <>
+            <div className="user-name">{currentUserData.userName}</div>
+            <NavLink
+              className="profile-icon"
+              activeClassName="profile-link-active"
+              to="/profile"
+            >
+              <i className="fas fa-user-circle"></i>
+            </NavLink>
+          </>
         )}
       </div>
       <hr />
