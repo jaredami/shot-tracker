5e37f2841b1995e19828cae178fbbe94f3da933d
Jared Storm <jaredrstorm@gmail.com>
2021-10-29 11:18:13 -0400

    Improve modal styles

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,19 +4,21 @@ import "./Modal.css";
 export default function Modal({ message, onConfirm, onCancel }) {
   return (
     <div className="modal__container">
-      <p className="modal__message">{message ? message : "Are you sure?"}</p>
-      <button
-        className="modal__button modal__button--yes"
-        onClick={() => onConfirm()}
-      >
-        Yes
-      </button>
-      <button
-        className="modal__button modal__button--no"
-        onClick={() => onCancel()}
-      >
-        No
-      </button>
+      <div className="modal">
+        <p className="modal__message">{message ? message : "Are you sure?"}</p>
+        <button
+          className="modal__button modal__button--yes"
+          onClick={() => onConfirm()}
+        >
+          Yes
+        </button>
+        <button
+          className="modal__button modal__button--no"
+          onClick={() => onCancel()}
+        >
+          No
+        </button>
+      </div>
     </div>
   );
 }
