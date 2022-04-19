75ebdd13381663d8613e7e5ee31400601726c57c
Jared Storm <jaredrstorm@gmail.com>
2022-04-19 11:29:58 -0400

    Update email with error handling

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,10 +4,12 @@ import { useHistory } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import LoadingIndicator from "./LoadingIndicator";
 import "./Profile.css";
+import Toast from "./Toast";
 
 export default function Dashboard() {
   const [error, setError] = useState("");
-  const { currentUser, logout, currentUserData, loading } = useAuth();
+  const { currentUser, logout, currentUserData, loading, updateEmail } =
+    useAuth();
   const { register, handleSubmit, formState, reset } = useForm({
     defaultValues: {
       userName: currentUserData?.userName,
@@ -15,6 +17,7 @@ export default function Dashboard() {
     },
   });
   const history = useHistory();
+  const [toast, setToast] = useState(null);
 
   useEffect(() => {
     reset({
@@ -39,6 +42,25 @@ export default function Dashboard() {
     console.log("userName", userName);
     console.log("email", email);
     // TODO: submit profile data updates here
+    return updateEmail(email)
+      .then((resp) => {
+        console.log("resp", resp);
+        // db.collection("users")
+        //   .doc(resp.user.uid)
+        //   .set({ email: resp.user.email, userName: userName });
+      })
+      .catch((error) => {
+        console.error("error", error);
+        setToast({
+          message:
+            error.message ??
+            "There was a problem updating your profile. Please try again.",
+          type: "error",
+        });
+        setTimeout(() => {
+          setToast(null);
+        }, 5000);
+      });
   }
 
   return (
@@ -46,54 +68,59 @@ export default function Dashboard() {
       {loading ? (
         <LoadingIndicator />
       ) : (
-        <div className="profile-container">
-          <div className="profile-grid">
-            <div className="stat-container">
-              <p className="stat">{currentUserData?.totalSessions || "0"}</p>
-              <p className="stat-label">Sessions</p>
+        <>
+          <div className="profile-container">
+            <div className="profile-grid">
+              <div className="stat-container">
+                <p className="stat">{currentUserData?.totalSessions || "0"}</p>
+                <p className="stat-label">Sessions</p>
+              </div>
+              <div className="stat-container">
+                <p className="stat">
+                  {currentUserData?.totalShotsMade || "-"}/
+                  {currentUserData?.totalShotsTaken || "-"}
+                </p>
+                <p className="stat-label">Shots Made</p>
+              </div>
+              <div className="stat-container">
+                <p className="stat">
+                  {currentUserData?.totalPercentage || "-"}
+                </p>
+                <p className="stat-label">Percentage</p>
+              </div>
+              <div className="stat-container">
+                <p className="stat">{currentUserData?.bestStreakEver || "-"}</p>
+                <p className="stat-label">Best Streak</p>
+              </div>
             </div>
-            <div className="stat-container">
-              <p className="stat">
-                {currentUserData?.totalShotsMade || "-"}/
-                {currentUserData?.totalShotsTaken || "-"}
-              </p>
-              <p className="stat-label">Shots Made</p>
-            </div>
-            <div className="stat-container">
-              <p className="stat">{currentUserData?.totalPercentage || "-"}</p>
-              <p className="stat-label">Percentage</p>
-            </div>
-            <div className="stat-container">
-              <p className="stat">{currentUserData?.bestStreakEver || "-"}</p>
-              <p className="stat-label">Best Streak</p>
-            </div>
-          </div>
 
-          {/* TODO either copy styles in Profile.css or extract shared Form component */}
-          <div className="login-container">
-            <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
-              {error && <div>{error}</div>}
-              <div className="login-form-row" id="user-name">
-                <label>User Name</label>
-                <input type="text" {...register("userName")} />
-              </div>
-              <div className="login-form-row" id="email">
-                <label>Email</label>
-                <input type="email" {...register("email")} />
-              </div>
-              <button
-                disabled={!formState.isDirty}
-                className="logout-btn"
-                type="submit"
-              >
-                Save
-              </button>
-              <button className="logout-btn" onClick={handleLogout}>
-                Log Out
-              </button>
-            </form>
+            {/* TODO either copy styles in Profile.css or extract shared Form component */}
+            <div className="login-container">
+              <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
+                {error && <div>{error}</div>}
+                <div className="login-form-row" id="user-name">
+                  <label>User Name</label>
+                  <input type="text" {...register("userName")} />
+                </div>
+                <div className="login-form-row" id="email">
+                  <label>Email</label>
+                  <input type="email" {...register("email")} />
+                </div>
+                <button
+                  disabled={!formState.isDirty}
+                  className="logout-btn"
+                  type="submit"
+                >
+                  Save
+                </button>
+                <button className="logout-btn" onClick={handleLogout}>
+                  Log Out
+                </button>
+              </form>
+            </div>
           </div>
-        </div>
+          {toast && <Toast message={toast.message} type={toast.type} />}
+        </>
       )}
     </>
   );
