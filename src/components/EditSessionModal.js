5718c5586c7efce50817fc64f8cd8644487c7276
Jared Storm <jaredrstorm@gmail.com>
2022-06-12 11:28:17 -0400

    Hook inputs up to useForm

--- src/components/EditSessionModal.js
+++ src/components/EditSessionModal.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import * as styles from "./EditSessionModal.module.css";
 
 export default function EditSessionModal({
@@ -6,6 +7,12 @@ export default function EditSessionModal({
   onCancel,
   sessionBeingEdited,
 }) {
+  const { register } = useForm({
+    defaultValues: {
+      shotsMade: sessionBeingEdited.shotsMade,
+      shotsTaken: sessionBeingEdited.shotsTaken,
+    },
+  });
   return (
     <div className={styles.modal__container}>
       <div className={styles.modal}>
@@ -14,14 +21,14 @@ export default function EditSessionModal({
           id="user-name"
         >
           <label>Shots Made</label>
-          <input type="text" value={sessionBeingEdited.shotsMade} />
+          <input type="text" {...register("shotsMade")} />
         </div>
         <div
           className={`login-form-row ${styles.modal__inputContainer}`}
           id="user-name"
         >
           <label>Shots Taken</label>
-          <input type="text" value={sessionBeingEdited.shotsTaken} />
+          <input type="text" {...register("shotsTaken")} />
         </div>
         <button
           className={[styles.modal__button, styles.modal__buttonYes].join(" ")}
