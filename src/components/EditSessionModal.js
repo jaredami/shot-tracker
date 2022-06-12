0885d1b39146e142021bdfd927af38e93e9aa03f
Jared Storm <jaredrstorm@gmail.com>
2022-06-11 22:27:43 -0400

    Convert EditSessionModal styles to module; display shots made and taken

--- src/components/EditSessionModal.js
+++ src/components/EditSessionModal.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./EditSessionModal.css";
+import * as styles from "./EditSessionModal.module.css";
 
 export default function EditSessionModal({
   onSave,
@@ -7,17 +7,24 @@ export default function EditSessionModal({
   sessionBeingEdited,
 }) {
   return (
-    <div className="modal__container">
-      <div className="modal">
-        <p className="modal__message">{sessionBeingEdited?.id}</p>
+    <div className={styles.modal__container}>
+      <div className={styles.modal}>
+        <div className="login-form-row" id="user-name">
+          <label>Shots Made</label>
+          <input type="text" value={sessionBeingEdited.shotsMade} />
+        </div>
+        <div className="login-form-row" id="user-name">
+          <label>Shots Taken</label>
+          <input type="text" value={sessionBeingEdited.shotsTaken} />
+        </div>
         <button
-          className="modal__button modal__button--yes"
+          className={[styles.modal__button, styles.modal__buttonYes].join(" ")}
           onClick={() => onSave()}
         >
           Save
         </button>
         <button
-          className="modal__button modal__button--no"
+          className={[styles.modal__button, styles.modal__buttonNo].join(" ")}
           onClick={() => onCancel()}
         >
           Cancel
